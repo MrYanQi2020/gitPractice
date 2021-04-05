9822b39bf5108669a32e0042e146fe32290ccc5a
mryanqi2020 <mryanqi2019@163.com>
2021-04-05 22:51:37 +0800

    初始化文件

---
+++ indes.js
@@ -0,0 +1,5 @@
+const body = document.body;
+function changeBody(){
+    body.style.background = "#f40";
+}
+changeBody();
---
+++ index.js
@@ -0,0 +1,7 @@
+const btn = document.getElementsByTagName('button')[0];
+btn.addEventListener('click',()=>{
+    btnfn();
+})
+function btnfn(){
+    console.log('i am dev branch');
+}
